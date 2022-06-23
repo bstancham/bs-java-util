386006d2f7b01c190c7d3506f7a5724629573371
bstancham <ben@bschambers.info>
2022-06-23 12:24:26 +0100

    update for java 11, junit 5 (jupiter) and gradle 7.4.2

---
+++ src/test/java/info/bschambers/bsutil/BSArraysTest.java
@@ -0,0 +1,12 @@
+package info.bschambers.bsutil;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BSArraysTest {
+    
+    @Test
+    public void testReverse() {
+        // fail();
+    }
+}
